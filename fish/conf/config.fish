32d58eedf10f302be082f6dfad681e7395fbbbe2
Nadav Cohen <ncohen@netflix.com>
2017-01-13 00:19:16 -0800

    More

--- fish/conf/config.fish
+++ fish/conf/config.fish
@@ -1,6 +1,8 @@
 set -x PATH ~/bin $PATH
 set -x PATH ~/.rbenv/shims $PATH
-set PATH $HOME/.jenv/bin $PATH
+set -x PATH $HOME/.jenv/bin $PATH
+
 set -x SCRIPT_WRAPPED_NEWT /tmp
+set -x GOPATH ~/dev/gopath
 
 status --is-interactive; and source (rbenv init -|psub)
---
+++ fish/conf/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/ncohen/.local/share/omf/themes/bobthefish/fish_prompt.fish
