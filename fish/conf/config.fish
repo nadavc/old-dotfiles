0bb621f13fb78a70238965b821bccd4f88959bd8
Nadav Cohen <ncohen@netflix.com>
2017-02-19 01:13:43 -0800

    Add binding for edit_command_buffer

--- fish/conf/config.fish
+++ fish/conf/config.fish
@@ -10,3 +10,9 @@ status --is-interactive; and source (rbenv init -|psub)
 alias npm="newt exec npm --"
 alias node="newt exec node --"
 alias yarn="newt exec yarn --"
+
+set -x EDITOR (which vim)
+
+function fish_user_key_bindings 
+  bind \ck edit_command_buffer 
+end
